388d8dd143918a7f7f7aa6411aa37059604d30ab
Phil Hauser <phil@zweck.co>
2019-04-14 14:54:47 +0100

    Update endpoint to dev server and correct form names in referrals

--- src/services/globalstorage/globalstorage.ts
+++ src/services/globalstorage/globalstorage.ts
@@ -9,7 +9,7 @@ export class GlobalStorage {
     loginVars: false,
     type: ""
   };
-  public primaryDomain: string = 'https://app.refersquare.co.uk';
+  public primaryDomain: string = 'http://139.59.178.1:4000/';
   // public primaryDomain: string = 'http://refersquare-dev.businessapps.co.uk';
   public baseUrl: string = `${this.primaryDomain}/api`;
   public user: any;
