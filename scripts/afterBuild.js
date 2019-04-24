c41765269cd8254161a4c5190eae1d8e80e7c70b
Phil Hauser <phil@zweck.co>
2019-04-24 19:41:47 +0100

    Add build script to auto gen ghpages

--- scripts/afterBuild.js
+++ scripts/afterBuild.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const shell = require('child_process').execSync;
 const fs = require('fs');
 const path = require('path');
@@ -5,7 +7,5 @@ const path = require('path');
 const src = 'platforms/browser/www';
 const dist = 'docs';
 
-module.exports = () => {
-  shell('mkdir -p ${dist}');
-  shell('cp -r ${src}/* ${dist}');
-}
+shell(`mkdir -p ${dist}`);
+shell(`cp -r ${src}/* ${dist}`);
